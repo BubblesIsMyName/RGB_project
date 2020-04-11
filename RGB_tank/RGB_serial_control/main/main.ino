3ccd68c3b74589c2122e864539c982251ddeff34
Gatis <gatis@pop-os.localdomain>
2020-04-11 01:16:28 +0100

    Read serial string of format RxGxBx (e.g.  255x255x255x) and the uC sets that colour

--- RGB_tank/RGB_serial_control/main/main.ino
+++ RGB_tank/RGB_serial_control/main/main.ino
@@ -11,6 +11,7 @@
 #define SET_BIT_LO(V, B) (V) &= (uint8_t)~BIT(B)
 
 #define PAUSE  1000     // msec
+#define LONGPAUSE  3000     // msec
 #define DELAY    10	// msec
 
 // Define the output function, using pin 0 on port b. -> pin D8 arduino
@@ -21,9 +22,10 @@ const byte numChars = 32;
 char receivedChars[numChars]; // an array to store the received data
 uint8_t pixel_value = 0;
 boolean newData = false, setup_flag = false;
+int string_lenth = 0;
 
 // Drive the three pixels in an infinit loop.
-void threepixeldemo(uint8_t pixel_v)
+void threepixeldemo(uint8_t pixel_r,uint8_t pixel_g,uint8_t pixel_b)
 {
   // Configure pin for output.
   SET_BIT_HI(DDRB, 0);
@@ -35,24 +37,30 @@ void threepixeldemo(uint8_t pixel_v)
   RGB_t rgb[1] = {{0,0,0}};
   WS2811RGB(rgb, ARRAYLEN(rgb));
   _delay_ms(PAUSE);
-// Red
-  rgb[0].r = pixel_v;
-  rgb[0].g = 0;
-  rgb[0].b = 0;
-  WS2811RGB(rgb, ARRAYLEN(rgb));
-  _delay_ms(PAUSE);
-// Green
-  rgb[0].r = 0;
-  rgb[0].g = pixel_v;
-  rgb[0].b = 0;
-  WS2811RGB(rgb, ARRAYLEN(rgb));
-  _delay_ms(PAUSE);
-// Blue
-  rgb[0].r = 0;
-  rgb[0].g = 0;
-  rgb[0].b = pixel_v;
-  WS2811RGB(rgb, ARRAYLEN(rgb));
-  _delay_ms(PAUSE);
+  // Red
+    rgb[0].r = pixel_r;
+    rgb[0].g = pixel_g;
+    rgb[0].b = pixel_b;
+    WS2811RGB(rgb, ARRAYLEN(rgb));
+    _delay_ms(LONGPAUSE);
+// // Red
+//   rgb[0].r = pixel_v;
+//   rgb[0].g = 0;
+//   rgb[0].b = 0;
+//   WS2811RGB(rgb, ARRAYLEN(rgb));
+//   _delay_ms(PAUSE);
+// // Green
+//   rgb[0].r = 0;
+//   rgb[0].g = pixel_v;
+//   rgb[0].b = 0;
+//   WS2811RGB(rgb, ARRAYLEN(rgb));
+//   _delay_ms(PAUSE);
+// // Blue
+//   rgb[0].r = 0;
+//   rgb[0].g = 0;
+//   rgb[0].b = pixel_v;
+//   WS2811RGB(rgb, ARRAYLEN(rgb));
+//   _delay_ms(PAUSE);
 }
 
 
@@ -74,21 +82,73 @@ void recvWithEndMarker() {
    }
    else {
      receivedChars[ndx] = '\0'; // terminate the string
+     string_lenth = ndx;
      ndx = 0;
      newData = true;
    }
  }
 }
 
+// void showNewData() {
+//  if (newData == true) {
+//    Serial.print("This just in ... ");
+//    Serial.println(receivedChars);
+//
+//    pixel_value = atoi(receivedChars);
+//    Serial.println(pixel_value);
+//    int strin_lentth = sizeof(receivedChars);
+//    Serial.println(strin_lentth);
+//
+//    newData = false;
+//    threepixeldemo(pixel_value);
+//  }
+// }
+
+
 void showNewData() {
  if (newData == true) {
- Serial.print("This just in ... ");
- Serial.println(receivedChars);
- // pixel_value = (uint8_t)receivedChars;
- pixel_value = atoi(receivedChars);
- Serial.println(pixel_value);
- newData = false;
- threepixeldemo(pixel_value);
+   uint8_t rd_pix = 0;
+   uint8_t gr_pix = 0;
+   uint8_t bl_pix = 0;
+   char ard_pix[20];
+   char agr_pix[20];
+   char abl_pix[20];
+   int rd=0,gr=0,bl=0;
+
+   Serial.print("This just in ... ");
+   Serial.println(receivedChars);
+
+   Serial.print("String lenth ... ");
+   Serial.println(string_lenth);
+
+   for(int i = 0; i <= string_lenth; i++){
+     if(receivedChars[i]!='x' && rd_pix == 0){
+       ard_pix[rd] = receivedChars[i];
+       rd++;
+       if(receivedChars[i+1]=='x')rd_pix = 1;
+     }
+     else if(receivedChars[i]!='x' && gr_pix == 0){
+       agr_pix[gr] = receivedChars[i];
+       gr++;
+       if(receivedChars[i+1]=='x')gr_pix = 1;
+     }
+     else if(receivedChars[i]!='x' && bl_pix == 0){
+       abl_pix[bl] = receivedChars[i];
+       bl++;
+       if(receivedChars[i+1]=='x')bl_pix = 1;
+     }
+   }
+
+  rd_pix = atoi(ard_pix);
+  gr_pix = atoi(agr_pix);
+  bl_pix = atoi(abl_pix);
+
+  Serial.println(rd_pix);
+  Serial.println(gr_pix);
+  Serial.println(bl_pix);
+
+   newData = false;
+   threepixeldemo(rd_pix,gr_pix,bl_pix);
  }
 }
 
